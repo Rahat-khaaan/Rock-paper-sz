04a5af43b78b6face5f0c2ee3ba2e4b72cbf4b61
Rahat-khaaan <136272096+Rahat-khaaan@users.noreply.github.com>
2023-08-16 13:49:59 -0700

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,82 @@
+const score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    tie: 0,
+};
+
+// The || OR oprator will check that which condition is true then it will run that
+
+updateScoreElement();
+
+let computerMove = '';
+
+function playGame(playerMove) {
+    let result = '';
+    if (playerMove === 'scissor') {
+        if (computerMove === 'rock') {
+            result = 'You lose';
+        } else if (computerMove === 'paper') {
+            result = 'You win';
+        } else if (computerMove === 'scissor') {
+            result = 'Tie';
+        }
+
+    } else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            result = 'You win';
+        } else if (computerMove === 'paper') {
+            result = 'Tie';
+        } else if (computerMove === 'scissor') {
+            result = 'You lose';
+        }
+    } else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie';
+        } else if (computerMove === 'paper') {
+            result = 'You lose';
+        } else if (computerMove === 'scissor') {
+            result = 'You win';
+        }
+    }
+
+    if (result === 'You win') {
+        score.wins += 1;
+    } else if (result === 'You lose') {
+        score.losses += 1;
+    } else if (result === 'Tie') {
+        score.tie += 1;
+    }
+
+    localStorage.setItem('score', JSON.stringify(score));  //now get the score out of local storage using GetItem at starting of script
+
+
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You 
+<img src="images/${playerMove}-emoji.png" class="move-icon">
+<img src="images/${computerMove}-emoji.png" class="move-icon">
+Computer`;
+}
+
+function updateScoreElement() {
+
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, losses: ${score.losses}, tie: ${score.tie}`;
+}
+
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = 'scissor';
+    }
+
+    console.log(computerMove);
+}
